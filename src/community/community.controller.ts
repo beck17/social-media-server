d3b393fdd4f586b1612c94761dafe2212ca663d2
beck17 <bahcha1122001@gmail.com>
2023-10-30 20:48:13 +0300

    community service and controller add CRUD (empty)

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { CommunityService } from './community.service';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('community')
 export class CommunityController {
-  constructor(private readonly communityService: CommunityService) {}
+	constructor(private readonly CommunityService: CommunityService) {}
+
+	@Auth()
+	@Get()
+	getAllCommunities() {}
+
+	@Auth()
+	@Get()
+	getOneCommunity() {}
+
+	@Auth()
+	@Post()
+	createCommunity() {}
+
+	@Auth()
+	@Put()
+	updateCommunity() {}
+
+	@Auth()
+	@Delete()
+	deleteCommunity() {}
 }
--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { CommunityModel } from './community.model';
+import { ModelType } from '@typegoose/typegoose/lib/types';
 
 @Injectable()
-export class CommunityService {}
+export class CommunityService {
+	constructor(
+		@InjectModel(CommunityModel)
+		private readonly CommunityModel: ModelType<CommunityModel>,
+	) {}
+
+	async getAllCommunities() {}
+
+	async getOneCommunity() {}
+
+	async createCommunity() {}
+
+	async updateCommunity() {}
+
+	async deleteCommunity() {}
+}
