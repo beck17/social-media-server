47eb29f5a044e564daef18f6485311a21407438b
beck17 <bahcha1122001@gmail.com>
2025-03-20 17:04:58 +0300

    add rooms for WS

--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -1,4 +1,5 @@
 import {
+	ConnectedSocket,
 	MessageBody,
 	SubscribeMessage,
 	WebSocketGateway,
@@ -20,27 +21,46 @@ export class MessageGateway {
 	@WebSocketServer()
 	server
 
+	@SubscribeMessage('join-conversation')
+	handleJoinConversation(
+		@ConnectedSocket() client,
+		@MessageBody() conversationId: string,
+	) {
+		client.join(conversationId)
+	}
+
+	@SubscribeMessage('leave-conversation')
+	handleLeaveConversation(
+		@ConnectedSocket() client,
+		@MessageBody() conversationId: string,
+	) {
+		client.leave(conversationId)
+	}
+
 	@SubscribeMessage('message:get')
-	async getConversation(@MessageBody('conversationId') conversationId: Types.ObjectId) {
+	async getConversation(
+		@ConnectedSocket() client,
+		@MessageBody('conversationId') conversationId: Types.ObjectId,
+	) {
 		const conversation = await this.ConversationService.getById(
 			new Types.ObjectId(conversationId),
 		)
-		await this.server.emit('conversation', conversation)
+
+		client.to(conversationId).emit(`conversation:${conversationId}`, conversation)
 	}
 
 	@SubscribeMessage('message:add')
-	async addMessage(@MessageBody() dto: MessageDto) {
+	async addMessage(
+		@ConnectedSocket() client,
+		@MessageBody() dto: MessageDto,
+	) {
 		await this.MessageService.createMessage(
 			new Types.ObjectId(dto.userFrom),
 			dto,
 		)
-		await this.getConversation(dto.conversationId)
-	}
 
-	// @SubscribeMessage('message')
-	// async handleMessage(
-	// 	@MessageBody('conversationId') conversationId: Types.ObjectId,
-	// ) {
-	// 	await this.server.emit('message', conversationId)
-	// }
+		client.to(dto.conversationId).emit(`conversation:${dto.conversationId}`,
+			await this.ConversationService.getById(dto.conversationId),
+		)
+	}
 }
